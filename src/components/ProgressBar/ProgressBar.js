233237e55c3d5253a4d035d3111750d9acbe2da1
Elijah Miller <eli@esmcustomcreations.com>
2024-08-26 17:02:22 -0400

    progress bar in progress

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,51 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  "small": {
+    height: "8px",
+    padding: "0",
+    radius: "4px"
+  },
+  "medium": {
+    height: "12px",
+    padding: "0",
+    radius: "4px"
+  },
+  "large": {
+    height: "16px",
+    padding: "4px",
+    radius: "4px"
+  }
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = STYLES[size];
+
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+    >
+      <Bar
+        style={{
+          '--width': value + "%",
+          '--height': styles.height,
+        }}
+      />
+    </Wrapper>
+  )
 };
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background-color: ${COLORS.primary};
+`
 
 export default ProgressBar;
