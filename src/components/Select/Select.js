9aba610c6e0ed84a59f708406b1d705d23431718
Elijah <eli.miller@protonmail.com>
2024-08-28 01:23:39 +0000

    select in progress

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,42 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <ValueWrapper>{value}</ValueWrapper>
+      <CustomIcon id="chevron-down" size={24} strokeWidth={2} />
+      <SelectWrapper value={value} onChange={onChange}>
+        {children}
+      </SelectWrapper>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  border: 1px solid ${COLORS.transparentGray35};
+  width: fit-content;
+  padding: 8px 4px 8px 16px;
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+`
+
+const ValueWrapper = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 24px;
+`
+
+const SelectWrapper = styled.select`
+  position: absolute;
+  opacity: 0;
+  left: 0;
+  background-color: ${COLORS.transparentGray35};
+`
+
+const CustomIcon = styled(Icon)`
+  display: inline-block;
+  vertical-align: middle;
+  /* right: 10px; */
+`
 
 export default Select;
