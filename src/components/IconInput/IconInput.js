87632993d08e32fc442db406e0d2c5b9e08a895d
Elijah <eli.miller@protonmail.com>
2024-09-01 01:30:44 +0000

    project completed. I think...

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,21 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  small: {
+    iconSize: 16,
+    height: 24,
+    fontSize: 14,
+    borderThickness: 1,
+  },
+  large: {
+    iconSize: 24,
+    height: 36,
+    fontSize: 18,
+    borderThickness: 2,
+  },
+};
+
 const IconInput = ({
   label,
   icon,
@@ -13,30 +28,66 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
+  const styles = STYLES[size];
   return (
     <Wrapper>
-      <StyledIcon id="search" />
-      <StyledInput />
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ '--size': styles.iconSize + 'px' }}>
+        <Icon id={icon} size={styles.iconSize} />
+      </IconWrapper>
+      <StyledInput
+        style={{
+          '--width': width + 'px',
+          '--height': styles.height + 'px',
+          '--font-size': styles.fontSize / 16 + 'rem',
+          '--border-thickness': styles.borderThickness + 'px',
+        }}
+        placeholder={placeholder}
+      />
     </Wrapper>
   )
 };
 
-const Wrapper = styled.div`
+
+
+const Wrapper = styled.label`
   position: relative;
   margin-left: 20px;
-  border: 1px solid black;
+  display: block;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
 `
 
-const StyledIcon = styled(Icon)`
-  /* left: 0;
-  position: absolute; */
-  display: inline-block;
-`
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`;
 
 const StyledInput = styled.input`
-  /* left: 30px; */
-  position: absolute;
-  /* display: inline-block; */
+  padding-left: 30px;
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.gray700};
+  height: var(--height);
+  width: var(--width);
+  outline-color: Highlight;
+  outline-offset: 2px;
+  font-size: var(--font-size);
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+  &:hover {
+    &::placeholder {
+      color: black;
+    }
+    border-bottom-color: ${COLORS.black};
+  }
 `
 
 export default IconInput;
